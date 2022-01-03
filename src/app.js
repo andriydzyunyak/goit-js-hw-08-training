d6c7698a9547cb7e1058508f8c2d76d6374c5aba
Andriy Dzyunyak <andriy.dzyunyak@gmail.com>
2022-01-03 02:03:13 +0200

    training

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import validatePassword from '../js/validate-password';
+import { addUser } from '../js/api-service';
+
+//import qwe from '../js/validate-password';
+// import {
+//   fetchAllUsers,
+//   fetchUserById,
+//   updateUserById,
+//   x as value,
+//   y as name,
+// } from '../js/api-service';
+//import * as apiService from '../js/api-service';
+console.log(validatePassword('password'));
+addUser('Mango');
+// console.log(fetchAllUsers);
+// console.log(fetchUserById);
+// console.log(updateUserById);
+// console.log(value);
+// console.log(name);
+//console.log(apiService);
